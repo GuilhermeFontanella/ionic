764659af0f9864f8383a9720558d9eeacb895647
GuilhermeFontanella <gui.jmf@hotmail.com>
2022-08-08 00:00:44 -0300

    ajuste na parte de registro e calculo de hora

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,7 +1,14 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActionSheetController, ActionSheetOptions, IonModal, ToastController } from '@ionic/angular';
+import { ActionSheetController, ActionSheetOptions, AlertController, IonModal, ToastController } from '@ionic/angular';
 import { OverlayEventDetail } from '@ionic/core/components';
+
+export interface listData {
+    registrationDate: Date,
+    employeeId: number,
+    clockIn: string,
+    clockOut: string
+}
 
 @Component({
   selector: 'app-register',
@@ -14,7 +21,7 @@ export class RegisterPage implements OnInit {
   optionButton: ActionSheetOptions;
   message = 'This modal example uses triggers to automatically open a modal when the button is clicked.';
   name: string;
-  dateNow: any;
+  dateNow;
   newDate: any;
   isWeekday = (dateString: string) => {
     const date = new Date(dateString);
@@ -29,22 +36,26 @@ export class RegisterPage implements OnInit {
   maxDate = new Date().toISOString();
   valueCheckedNow: boolean = true;
   valueCheckedNew: boolean = false;
+  listData: listData[] = [];
+  justificative: boolean = false
   
 
   constructor(
     public actionSheetController: ActionSheetController,
     private toastController: ToastController,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private alert: AlertController
     ) {}
 
   ngOnInit() {
     this.cretaeForm();
+    this.dateNow = new Date();
   }
 
   cretaeForm() {
     this.form = this.formBuilder.group({
       employeeEnrollment: [null, [Validators.required, Validators.max(9999999)]],
-      dateSelected: [new Date().toUTCString()]
+      dateSelected: []
     })
   }
 
@@ -63,51 +74,102 @@ export class RegisterPage implements OnInit {
         icon: 'close-circle',
         data: 10,
         handler: () => {
-          console.log('Share clicked');
+          this.showModal();
         }
       }, {
         text: 'Off work',
         icon: 'game-controller',
         data: 'Data value',
         handler: () => {
-          console.log('Play clicked');
+          this.showModal();
         }
       }, {
         text: 'Holiday',
         icon: 'bed',
         handler: () => {
-          console.log('Favorite clicked');
+          this.showModal();
         }
       }, {
         text: 'Vacation',
         icon: 'airplane',
         handler: () => {
-          console.log('Cancel clicked');
+          this.showModal();
         },
       }, {
         text: 'Cancel',
         icon: '',
         role: 'cancel',
-        handler: () => {
-          console.log('Cancel clicked');
-        },
+        handler: () => {},
       }]
     });
     await actionSheet.present();
-
     const { role, data } = await actionSheet.onDidDismiss();
-    console.log('onDidDismiss resolved with role and data', role, data);
   }
 
-  async saveData() {
+  async showToast(color: string, duration: number, icon: string, message: string) {
     const toast = await this.toastController.create({
-      color: 'dark',
-      duration: 3000,
-      icon: 'checkmark-circle',
-      message: 'Your Attendance was registered!',
+      color,
+      duration,
+      icon,
+      message,
     });
-
     await toast.present();
+  }
+  
+  saveData() {
+    const employeeId = this.form.get('employeeEnrollment').value;
+    let clockIn;
+    let clockOut;
+    let timeDiff;
+
+    if (!this.listData.length) {
+      this.listData.push({
+        registrationDate: new Date(),
+        employeeId: employeeId,
+        clockIn: this.form.get('dateSelected')?.value ? this.form.get('dateSelected')?.value : new Date(),
+        clockOut: null
+      })
+      timeDiff = Date.parse(this.form.get('dateSelected')?.value); 
+      this.showToast(
+        'dark',
+        3000,
+        'checkmark-circle',
+        'Your Attendance was registered!'
+      );
+      this.closeModal();
+      console.log(this.listData);
+    } else {
+      let clockIn = this.listData[0].clockIn;
+      let out = Date.parse(this.form.get('dateSelected').value);
+      let diff = out - Date.parse(clockIn);
+      if (diff <= 21600000) {
+        console.log('entrou aqui diff < 6h')
+        clockOut = this.form.get('dateSelected').value ? this.form.get('dateSelected')?.value : new Date(),
+        this.listData.push({
+          registrationDate: new Date(),
+          employeeId,
+          clockIn,
+          clockOut
+        })
+        this.showToast(
+          'dark',
+          3000,
+          'checkmark-circle',
+          'Your Attendance was registered!'
+        );
+        this.closeModal();
+      }
+      else {
+        this.presentAlert(
+          'Alert',
+          'Justificative needed!',
+          'You must enter a Justificative.',
+          ['OK']
+        );
+        this.justificative = true;
+        return;
+      }
+    };
     this.cretaeForm();
   }
 
@@ -115,16 +177,16 @@ export class RegisterPage implements OnInit {
     this.modal.present();
   }
 
+  closeModal() {
+    this.modal.dismiss(this.name, 'confirm');
+  }
+
   cancel() {
     this.modal.dismiss(null, 'cancel'); 
   }
 
   confirm() {
-    this.modal.dismiss(this.name, 'confirm');
     this.saveData();
-    console.log(this.form.value)
-    console.log(this.dateNow);
-    console.log(this.newDate);
   }
 
   onWillDismiss(event: Event) {
@@ -135,7 +197,25 @@ export class RegisterPage implements OnInit {
   }
 
   setNewDate(event: any) {
+    this.dateNow = event.detail.value;
     this.form.get('dateSelected').setValue(event.detail.value);
+    console.log(this.form.get('dateSelected').setValue(event.detail.value));
+    
+  }
+
+  async presentAlert(
+    header: string, 
+    subHeader: string, 
+    message: string, 
+    buttons: any[]
+  ) {
+    const alert = await this.alert.create({
+      header,
+      subHeader,
+      message,
+      buttons,
+    });
+    await alert.present();
   }
 
 }
